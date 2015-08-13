f4306fc015f6ca9e477651570ce0ee86c93d1c34
Martin Hohenberg <martin.hohenberg@gmail.com>
2015-08-13 07:11:32 +0200

    Validation UI for easier testing

---
+++ LibraryTest/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SharpBTC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(12, 9);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(48, 13);
+            this.lblAddress.TabIndex = 0;
+            this.lblAddress.Text = "Address:";
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(66, 9);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(570, 20);
+            this.tbAddress.TabIndex = 1;
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(15, 44);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(702, 197);
+            this.tbOutput.TabIndex = 2;
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(642, 7);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(75, 23);
+            this.btnValidate.TabIndex = 3;
+            this.btnValidate.Text = "Validate!";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // LibraryTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 253);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Name = "LibraryTest";
+            this.Text = "LibraryTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button btnValidate;
+    }
+}
---
+++ LibraryTest/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SharpBTC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            tbOutput.Text = String.Empty;
+            try {
+                if (BTCValidator.ValidateAddress(tbAddress.Text, Prefix.Pubkey_hash))
+                {
+                    tbOutput.Text += "This is a valid Bitcoin Address. \r\n";
+                }
+            }
+            catch (Exception excp) {
+                tbOutput.Text += "This is NOT a valid Bitcoin Address. \r\n";
+            }
+
+            try {
+                if (BTCValidator.ValidateAddress(tbAddress.Text, Prefix.Testnet_pubkey_hash))
+                {
+                    tbOutput.Text += "This is a valid Testnet Address. \r\n";
+                }
+            }             
+            catch (Exception excp) {
+                tbOutput.Text += "This is NOT a valid Testnet Address. \r\n";
+            }
+
+            try {
+                if (BTCValidator.ValidateAddress(tbAddress.Text, Prefix.Script_hash))
+                {
+                    tbOutput.Text += "This is a valid Bitcoin Multisig Address. \r\n";
+                }
+            } catch (Exception excp) {
+                tbOutput.Text += "This is NOT a valid Bitcoin Multisig Address. \r\n";
+            }
+
+            try {
+                if (BTCValidator.ValidateAddress(tbAddress.Text, Prefix.Testnet_script_hash))
+                {
+                    tbOutput.Text += "This is a valid Testnet Multisig Address. \r\n";
+                }
+            }
+            catch (Exception excp) {
+                tbOutput.Text += "This is NOT a valid Testnet Multisig Address. \r\n";
+            }
+           
+        }
+    }
+}
